06da731ed57c930a5676a502ae10f97c305f3544
Kash2910 <kashif.sid2910@gmail.com>
2024-12-27 21:22:57 +0530

    Corrected base path in vite.config.js for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/quran-app/', // Replace with your repository name
-});
+  base: '/', 
